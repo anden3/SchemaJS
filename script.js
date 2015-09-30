57b6b7e96b608e88488f90764897010527f44f40
André Vennberg <mac@macs-Air.lan>
2015-09-30 18:21:02 +0200

    added settings, material design, header, and more

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var background = document.getElementById("schedule");
-
-Date.prototype.getWeek = function() {
+Date.prototype.getWeek = function () {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 }
@@ -9,14 +7,17 @@ Date.prototype.getActualDay = function() {
     return (this.getDay() + 6) % 7;
 }
 
-var getImage = function() {
-    var schoolID = "29540";
-    var userID = "980523-6032";
-    var week = (new Date()).getWeek();
-    var today = Math.pow(2, (new Date()).getActualDay());
+var background = document.getElementById("schedule");
+var settings = document.getElementById("settings");
 
-    var width = window.innerWidth - 8;
-    var height = window.innerHeight - 16;
+var schoolID = "29540";
+var userID = "980523-6032";
+var week = (new Date()).getWeek();
+var today = Math.pow(2, (new Date()).getActualDay());
+
+var getImage = function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight - 40;
 
     background.style.width = width;
     background.style.height = height;
@@ -24,8 +25,42 @@ var getImage = function() {
     return "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=" + schoolID + "/sv-se&type=-1&id=" + userID + "&period=&week=" + week + "&mode=1&printer=0&colors=32&head=0&clock=1&foot=0&day=" + today + "&width=" + width + "&height=" + height + "&maxwidth=" + width + "&maxheight=" + height;
 }
 
+var toggleSettings = function (toggle) {
+    if (toggle === 1) {
+        settings.style.display = "block";
+        background.style.webkitFilter = "blur(2px)";
+    }
+    else if (toggle === 0) {
+        settings.style.display = "none";
+        background.style.webkitFilter = "blur(0)";
+    }
+
+    background.onclick = function(e) {
+        if(e.target != settings) {
+            toggleSettings(0);
+        }
+    }
+}
+
 background.style.backgroundImage = "url(" + getImage() + ")";
 
 window.addEventListener("resize", function () {
     background.style.backgroundImage = "url(" + getImage() + ")";
 });
+
+document.getElementById("settingsButton").addEventListener("click", function () {
+    toggleSettings(1);
+});
+
+document.getElementById("settingsBackButton").addEventListener("click", function () {
+    toggleSettings(0);
+});
+document.getElementById("submitSettings").addEventListener("click", function () {
+    schoolID = document.getElementById("schoolID").value;
+    userID = document.getElementById("userID").value;
+    week = document.getElementById("week").value;
+    day = document.getElementById("day").value;
+
+    background.style.backgroundImage = "url(" + getImage() + ")";
+    toggleSettings(0);
+})
