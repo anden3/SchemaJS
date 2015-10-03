9745a9858f7646ca211386c52cabb762e661fb75
André Vennberg <mac@macs-Air.lan>
2015-10-03 15:28:28 +0200

    added mySQL integration

---
+++ store_foods.php
@@ -0,0 +1,36 @@
+<?php
+
+if ( $_POST ) {
+    header('Content-Type: text/html; charset=utf8mb4_swedish_ci');
+
+    $con = mysqli_connect("localhost", "root", "HU6oeiUWpkQ3j0Yssg8X", "matsedel");
+
+    mysqli_set_charset($con,"utf8mb4");
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $week = $_POST['week'];
+    $day = $_POST['day'];
+    $desc = $_POST['desc'];
+    $key = $_POST['key'];
+
+    $week = mysqli_real_escape_string($con, $week);
+    $day = mysqli_real_escape_string($con, $day);
+    $desc = mysqli_real_escape_string($con, $desc);
+    $key = mysqli_real_escape_string($con, $key);
+
+    $sql="INSERT INTO food (Week, Day, Mat, PrimaryKey)
+    VALUES ('$week', '$day', '$desc', '$key')";
+
+    if (!mysqli_query($con, $sql)) {
+        die('Error: ' . mysqli_error($con));
+    }
+
+    echo "Record added";
+
+    mysqli_close($con);
+}
+
+?>
