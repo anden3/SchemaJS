5dfe6add5c443f8308314039599e0f00aa8e197c
André Vennberg <mac@macs-MacBook-Air.local>
2015-10-10 18:05:10 +0200

    fixed getWeek prototype, fixed cookie and localStorage behavior, compressed some statements into for-loops, allowed greated variety of option formats, commented code some more, made formatting of food descriptions better, removed unnecessary css rule, fixed iPhone specific bug, and beautified code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//Adds method for getting week number
-Date.prototype.getWeek = function () {
-    var onejan = new Date(this.getFullYear(), 0, 1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-}
-
 //Adds method for getting day of the week, where the first day of the week is Monday instead of Sunday
 Date.prototype.getActualDay = function () {
     return (this.getDay() + 6) % 7;
-}
+};
+
+//Adds method for getting week number
+Date.prototype.getWeek = function () {
+    var onejan = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getActualDay()) / 7);
+};
 
 //Adding default variables and values
 var header = document.getElementById("header"),
@@ -16,26 +16,27 @@ var header = document.getElementById("header"),
     settings = document.getElementById("settings"),
     days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Vecka"],
     settingsVisible = false,
-    weeksAdded = 0,
     daysAdded = 0,
     foodWeeks = {},
     foodDays = {},
     foodDescs = [],
     food = {},
-    primaryKey = 0;
+    primaryKey = 0,
+    replaceChars = [["Ã¥", "Ã¥", "Ã¤", "Ã¶", "Ã©", "Ã¶", "Ã¤", "Ã–"], ["å", "å", "ä", "ö", "é", "ö", "ä", "Ö"]],
+    values = ["IDType", "schoolID", "userID", "classID", "week"];
 
 //Function to create a cookie
 var createCookie = function (name, value, days) {
+    var expires = " ";
+
     if (days) { //If days is defined, create regular cookie
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = "; expires=" + date.toGMTString();
-    } else {    //Else, create session cookie only
-        var expires = " ";
+        expires = "; expires=" + date.toGMTString();
     }
 
     document.cookie = name + "=" + value + expires + "; path=/";
-}
+};
 
 //Function to read a cookie
 var readCookie = function (name) {
@@ -53,37 +54,37 @@ var readCookie = function (name) {
             return c.substring(nameEQ.length, c.length);
         }
     }
-    return null;    //If cookie doesn't exist, return null
-}
+    return null; //If cookie doesn't exist, return null
+};
 
 //Updating the values with the ones stored in cookies or localstorage, as well as adding the current week and day
 var setDefaultValues = function () {
-    if (readCookie("schoolID") != null) {
+    if (readCookie("schoolID") !== null) {
         schoolID = readCookie("SCHOOLID");
-    } else if (localStorage.schoolID != null) {
+    } else if (localStorage.schoolID !== "undefined" && typeof localStorage.schoolID !== "undefined") {
         schoolID = localStorage.schoolID;
     } else {
         schoolID = "29540";
     }
 
-    if (readCookie("USERID") != null) {
+    if (readCookie("USERID") !== null) {
         userID = readCookie("USERID");
-    } else if (localStorage.userID != null) {
+    } else if (localStorage.userID !== "undefined" && typeof localStorage.userID !== "undefined") {
         userID = localStorage.userID;
     } else {
         userID = "";
     }
 
-    if (readCookie("CLASSID") != null) {
+    if (readCookie("CLASSID") !== null) {
         classID = readCookie("CLASSID");
-    } else if (localStorage.classID != null) {
+    } else if (localStorage.classID !== "undefined" && typeof localStorage.classID !== "undefined") {
         classID = localStorage.classID;
     } else {
         classID = "";
     }
 
     //Ensures the proper radio button is checked
-    if (readCookie("IDTYPE") != null) {
+    if (readCookie("IDTYPE") !== null) {
         IDType = readCookie("IDTYPE");
         if (IDType.length >= 10) {
             document.getElementById("userRadio").checked = true;
@@ -92,7 +93,7 @@ var setDefaultValues = function () {
             document.getElementById("classRadio").checked = true;
             document.getElementById("userRadio").checked = false;
         }
-    } else if (localStorage.IDType != null) {
+    } else if (localStorage.IDType !== "undefined" && typeof localStorage.IDType !== "undefined") {
         IDType = localStorage.IDType;
         if (IDType.length >= 10) {
             document.getElementById("userRadio").checked = true;
@@ -118,14 +119,13 @@ var setDefaultValues = function () {
         //Else, set week to current week
         week = parseInt(week);
     }
-}
+};
 
 //Update the values of the settings with the stored values
 var displayDefaultValues = function () {
-    document.getElementById("schoolID").value = schoolID;
-    document.getElementById("userID").value = userID;
-    document.getElementById("classID").value = classID;
-    document.getElementById("week").value = week;
+    for (var i = 1; i < values.length; i++) {
+        document.getElementById(values[i]).value = window[values[i]];
+    }
     document.getElementById("dayPicker").innerHTML = "<p>" + days[Math.log2(today)] + "</p>";
 }
 
@@ -146,7 +146,7 @@ var getImage = function (ID) {
 
     //Returns the image
     return "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=" + schoolID + "/sv-se&type=-1&id=" + ID + "&period=&week=" + week + "&mode=1&printer=0&colors=32&head=0&clock=1&foot=0&day=" + today + "&width=" + width + "&height=" + height + "&maxwidth=" + width + "&maxheight=" + height;
-}
+};
 
 //Function to toggle between displaying and hiding the settings
 var toggleSettings = function (toggle) {
@@ -166,7 +166,7 @@ var toggleSettings = function (toggle) {
         settings.style.display = "none";
         background.style.webkitFilter = "blur(0)";
     }
-}
+};
 
 //What happens when the Verkställ button is pressed, or the navigation keys/swipes are pressed/swiped
 var submitSettings = function (direction) {
@@ -174,6 +174,22 @@ var submitSettings = function (direction) {
     schoolID = document.getElementById("schoolID").value;
     userID = document.getElementById("userID").value;
     classID = document.getElementById("classID").value;
+
+    //Checks if the year in the userID is written using four numbers, and if so, decreases it to two numbers
+    if (userID.length > 11) {
+        userID = userID.substring(2, userID.length);
+        document.getElementById("userID").value = userID;
+    }
+
+    //Checks if the userID has a dash in it, and if not, it adds one
+    if (userID.substring(userID.length - 5, userID.length - 4) !== "-" && userID != "") {
+        userID = userID.substring(0, userID.length - 4) + "-" + userID.substring(userID.length - 4, userID.length);
+        document.getElementById("userID").value = userID;
+    }
+
+    //Converts classID to only uppercase letters
+    classID = classID.toUpperCase();
+    document.getElementById("classID").value = classID;
 
     //Checking which radio button is checked, and saving the corresponding id to IDType
     if (document.getElementById("userRadio").checked) {
@@ -183,15 +199,11 @@ var submitSettings = function (direction) {
     }
 
     //Saving the variables to cookies
-    createCookie("SCHOOLID", schoolID, 365);
-    createCookie("USERID", userID, 365);
-    createCookie("CLASSID", classID, 365);
-    createCookie("IDTYPE", IDType, 365);
-
-    localStorage.setItem("schoolID", schoolID);
-    localStorage.setItem("userID", userID);
-    localStorage.setItem("classID", classID);
-    localStorage.setItem("IDType", IDType);
+    for (var i = 0; i < values.length - 1; i++) {
+        localStorage.setItem(values[i], window[values[i]]);
+        var tempArray = values[i].toUpperCase();
+        createCookie(tempArray, window[values[i]], 365);
+    }
 
     //Getting the week from the number field
     week = parseInt(document.getElementById("week").value, 10);
@@ -247,7 +259,6 @@ var submitSettings = function (direction) {
         foodElement.style.display = "none";
         background.style.marginTop = "6vh";
         foodElement.innerHTML = "";
-
     }
 
     //Set the background image to the schedule
@@ -255,7 +266,7 @@ var submitSettings = function (direction) {
 
     //Hide the settings window
     toggleSettings(0);
-}
+};
 
 //Adding event listeners for different events
 var eventListeners = function () {
@@ -264,7 +275,6 @@ var eventListeners = function () {
         if (settingsVisible) {
             toggleSettings(1);
         }
-
         background.style.backgroundImage = "url(" + getImage(IDType) + ")";
     });
 
@@ -285,8 +295,8 @@ var eventListeners = function () {
 
         for (var i = 0; i < days.length; i++) {
             document.getElementById(days[i]).addEventListener("touchstart", function () {
-                var touchStart = event.target;
-                var day = event.srcElement.id;
+                var touchStart = event.target,
+                    day = event.srcElement.id;
 
                 document.getElementById(day).addEventListener("touchend", function () {
                     if (event.target === touchStart) {
@@ -317,10 +327,14 @@ var eventListeners = function () {
         });
 
         swipedetect(background, function (swipedir) {
-            submitSettings(swipedir);
+            if (swipedir != "none") {
+                submitSettings(swipedir);
+            }
         });
     } else { //If the client doesn't have a touch screen, check for click events instead of touchstart/end events
-        window.addEventListener("keydown", function () {
+
+        //Change the view based on which navigation key was pressed
+        $(window).keydown(function () {
             if (settingsVisible === false) {
                 if (event.keyCode === 37) {
                     submitSettings("left");
@@ -334,42 +348,49 @@ var eventListeners = function () {
             }
         });
 
-        document.getElementById("settingsButton").addEventListener("click", function () {
+        //Show the settings when pressing the settings button
+        $("#settingsButton").click(function () {
             toggleSettings(1);
         });
 
+        //Enable clicking on the days in the drop-down menu
         for (var i = 0; i < days.length; i++) {
-            document.getElementById(days[i]).addEventListener("click", function () {
+            $("#" + days[i]).click(function () {
                 var day = event.srcElement.id;
                 document.getElementById("dayPicker").innerHTML = "<p>" + day + "</p>";
             });
         }
 
-        document.getElementById("submitSettings").addEventListener("click", function () {
+        //Submit settings when pressing the submit button
+        $("#submitSettings").click(function () {
             submitSettings(0);
         });
 
-        document.getElementById("cancelSettings").addEventListener("click", function () {
-            submitSettings(0);
+        //Hide the settings window when pressing the cancel button
+        $("#cancelSettings").click(function () {
+            toggleSettings(0);
         });
 
+        //Save all text field elements to an array
         var textFields = document.getElementsByClassName("mdl-textfield__input");
 
+        //If enter is pressed while one of the textfields are edited, submit the settings
         for (var i = 0; i < textFields.length; i++) {
-            textFields[i].addEventListener("keydown", function () {
+            $(textFields[i]).keydown(function () {
                 if (event.keyCode === 13) {
                     submitSettings(0);
                 }
             });
         }
 
-        window.addEventListener("keydown", function () {
+        //If escape is pressed while the settings window is visible, hide the settings window
+        $(window).keydown(function () {
             if (settingsVisible && event.keyCode === 27) {
                 toggleSettings(0);
             }
         });
     }
-}
+};
 
 //Function for detecting swipes and their direction
 function swipedetect(el, callback) {
@@ -377,14 +398,11 @@ function swipedetect(el, callback) {
         swipedir,
         startX,
         startY,
-        distX,
-        distY,
         threshold = 100, //Required min distance traveled to be considered swipe
         restraint = 100, //Maximum distance allowed at the same time in perpendicular direction
         allowedTime = 500, //Maximum time allowed to travel that distance
-        elapsedTime,
         startTime,
-        handleswipe = callback || function (swipedir) {}
+        handleswipe = callback || function (swipedir) {};
 
     touchsurface.addEventListener('touchstart', function (e) {
         var touchobj = e.changedTouches[0],
@@ -404,9 +422,9 @@ function swipedetect(el, callback) {
     touchsurface.addEventListener('touchend', function (e) {
         var touchobj = e.changedTouches[0];
 
-        distX = touchobj.pageX - startX; //Get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY; //Get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime; //Get time elapsed
+        var distX = touchobj.pageX - startX, //Get horizontal dist traveled by finger while in contact with surface
+            distY = touchobj.pageY - startY, //Get vertical dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime; //Get time elapsed
 
         if (elapsedTime <= allowedTime) { //First condition for a swipe met
             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { //Second condition for horizontal swipe met
@@ -418,28 +436,25 @@ function swipedetect(el, callback) {
         handleswipe(swipedir);
         e.preventDefault();
     }, false);
-}
+};
 
-//Update the SQL-database with the data from the matsedel RSS-feed
+//Update the SQL-database with the data from the matsedel RSS-feed, should only be run once a week
 var parseRSS = function () {
-    var currentWeek = (new Date()).getWeek();
-    var weeksStored = currentWeek - 40; //Checking how many weeks has passed since the first week that's been stored
+    var currentWeek = (new Date()).getWeek(),
+        weeksStored = currentWeek - 34; //Checking how many weeks has passed since the first week that's been stored
 
     primaryKey = (weeksStored * 5); //Changing the primary key based on weeks stored, to allow new weeks to be added after the old ones
 
     //Getting the data from the proxy_file.php file
     $.get("proxy_file.php", function (data) {
         $(data).find("item").each(function () { //Looping for every item element in the data
-            var el = $(this);   //Setting el to the found element
-            var foodData;
+            var el = $(this); //Setting el to the found element
 
             //Saving the contents of the item element to an array
-            var title = el.find("title").text();
-            var titleItems = title.split(" ");
-
-            //Saving the week and day of the array to variables
-            var foodWeek = parseInt(titleItems[3]);
-            var foodDay = titleItems[0];
+            var title = el.find("title").text(),
+                titleItems = title.split(" "),
+                foodWeek = parseInt(titleItems[3]), //Saving the week and day of the array to variables
+                foodDay = titleItems[0];
 
             //Saving the description of the food to a variable
             var foodDescFull = el.find("description").text();
@@ -447,6 +462,17 @@ var parseRSS = function () {
             //If the description contains a line break, then remove the part before the line break
             if (foodDescFull.indexOf("<br/>") !== -1) {
                 foodDescFull = foodDescFull.substring(foodDescFull.indexOf("<br/>") + 5, foodDescFull.length);
+            }
+
+            //Removing spaces between eventual parentheses in the description
+            if (foodDescFull.indexOf("( ") !== -1) {
+                foodDescFull = foodDescFull.replace("( ", "(");
+                foodDescFull = foodDescFull.replace(" )", ")");
+            }
+
+            //Removing space after eventual forward slash
+            if (foodDescFull.indexOf("/ ") !== -1) {
+                foodDescFull = foodDescFull.replace("/ ", "/");
             }
 
             //Trimming away whitespace from the description
@@ -464,7 +490,7 @@ var parseRSS = function () {
             primaryKey += 1;
         });
     });
-}
+};
 
 //Function to get the foods from the SQL-database
 var getFoods = function () {
@@ -472,31 +498,28 @@ var getFoods = function () {
     $.get("get_foods.php", function (data) {
         foodData = data.match(/[^\r\n]+/g); //Splits the string into lines, and saves them to the foodData array
         for (var i = 0; i < foodData.length; i++) {
-            foodData[i] = foodData[i].replace("Ã¥", "å");   //Fixes broken Swedish characters
-            foodData[i] = foodData[i].replace("Ã¥", "å");
-            foodData[i] = foodData[i].replace("Ã¤", "ä");
-            foodData[i] = foodData[i].replace("Ã¶", "ö");
-            foodData[i] = foodData[i].replace("Ã©", "é");
-            foodData[i] = foodData[i].replace("Ã¶", "ö");
-            foodData[i] = foodData[i].replace("Ã¤", "ä");
+            //Fixes broken characters
+            for (var c = 0; c < replaceChars[0].length; c++) {
+                foodData[i] = foodData[i].replace(replaceChars[0][c], replaceChars[1][c]);
+            }
 
-            //Splits the line into words
+            //Splits the line into an array of words
             var foodDataSplit = foodData[i].split(" ");
 
-            //Adds the first word to the foodWeeks set
-            if (foodDataSplit[0] != "") {
+            //Adds the first word to the foodWeeks object
+            if (foodDataSplit[0] !== "") {
                 foodWeeks[foodDataSplit[0]] = true;
             }
 
-            //Removes the first word
+            //Removes the first word in the array
             foodDataSplit.shift();
 
-            //Adds the first word to the foodDays set
-            if (foodDataSplit[0] != "") {
+            //Adds the first word to the foodDays object
+            if (foodDataSplit[0] !== "") {
                 foodDays[foodDataSplit[0]] = true;
             }
 
-            //Removes the first word
+            //Removes the first word in the array
             foodDataSplit.shift();
 
             //Join all the unremoved words together as a string again and remove the leading and trailing character
@@ -504,12 +527,15 @@ var getFoods = function () {
             foodDesc = foodDesc.substring(1, foodDesc.length - 1);
 
             //If the description isn't a tab character, then push the description to the foodDescs array
-            if (foodDesc != "      ") {
+            /*if (foodDesc != "      ") {
                 foodDescs.push(foodDesc);
-            }
+            }*/
+
+            //Push description to the foodDescs array
+            foodDescs.push(foodDesc);
         }
 
-        //Converting the foodWeeks and foodDays sets to arrays
+        //Converting the foodWeeks and foodDays objects to arrays
         foodWeeks = Object.keys(foodWeeks);
         foodDays = Object.keys(foodDays);
 
@@ -519,12 +545,12 @@ var getFoods = function () {
                 foodDay = foodDays[x];
                 food[foodWeeks[y]] = {
                     foodDay: null
-                }
+                };
             }
         }
 
         //Iterate the foodDescs and foodWeeks arrays, and append the descriptions to the days in the food object
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < foodDays.length; i++) {
             for (var x = 0; x < foodWeeks.length; x++) {
                 food[foodWeeks[x]][foodDays[i]] = foodDescs[i + (5 * x)];
             }
@@ -532,7 +558,7 @@ var getFoods = function () {
         //Update the view
         submitSettings();
     });
-}
+};
 
 //Run all necessary functions before viewing the page
 setDefaultValues();
