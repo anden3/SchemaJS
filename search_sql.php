5e918169b0334acb3b4315c2ead084f1713501cb
André Vennberg <mac@macs-Air.lan>
2015-10-19 16:19:37 +0200

    changed sql queries to use prepared statements

--- search_sql.php
+++ search_sql.php
@@ -5,21 +5,21 @@ ini_set("default_charset", 'UTF-8');
 //Gets pass from ignored text file
 $pass = rtrim(file_get_contents("sql_pass.txt"));
 
+//Set proper header to reduce broken characters
+header('Content-Type: text/html; charset=utf8mb4_swedish_ci');
+
+//Connect to the SQL-database
+$con = mysqli_connect("mysql513.loopia.se", "98anve32@k132604", $pass, "kodlabb_se_db_6_db_7_db_2_db_13");
+
+//Set the correct charset
+mysqli_set_charset($con, "utf8mb4");
+
+//If the server failed to connect, echo an error message
+if (mysqli_connect_errno()) {
+    echo "error";
+}
+
 if ( $_POST ) {
-    //Set proper header to reduce broken characters
-    header('Content-Type: text/html; charset=utf8mb4_swedish_ci');
-
-    //Connect to the SQL-database
-    $con = mysqli_connect("mysql513.loopia.se", "98anve32@k132604", $pass, "kodlabb_se_db_6_db_7_db_2_db_13");
-
-    //Set the correct charset
-    mysqli_set_charset($con, "utf8mb4");
-
-    //If the server failed to connect, echo an error message
-    if (mysqli_connect_errno()) {
-        echo "error";
-    }
-
     //Save the sent variables to local variables
     $search = $_POST['data'];
     $search = mb_strtoupper($search, "UTF-8");
@@ -37,37 +37,32 @@ if ( $_POST ) {
 
     //Save the SQL-query as a string
     if ($table == "teachers") {
-        $query = "SELECT FullName, Name FROM $table
-        WHERE '$search' = SUBSTRING(UPPER(FirstName), 1, LENGTH('$search') - '$offset')
-        OR '$search' = SUBSTRING(UPPER(LastName), 1, LENGTH('$search') - '$offset')
-        OR '$search' = SUBSTRING(UPPER(FullName), 1, LENGTH('$search') - '$offset')
-        LIMIT 5";
+        $stmt = mysqli_prepare($con, "SELECT FullName, Name FROM $table WHERE ? = SUBSTRING(UPPER(FirstName), 1, LENGTH(?) - ?) OR ? = SUBSTRING(UPPER(LastName), 1, LENGTH(?) - ?) OR ? = SUBSTRING(UPPER(FullName), 1, LENGTH(?) - ?) LIMIT 5");
+
+        mysqli_stmt_bind_param($stmt, "ssississi", $search, $search, $offset, $search, $search, $offset, $search, $search, $offset);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $FullName, $Name);
     }
 
     else {
-        $query = "SELECT Name FROM $table
-        WHERE '$search' = SUBSTRING(UPPER(Name), 1, LENGTH('$search') - '$offset')
-        LIMIT 5";
+        $stmt = mysqli_prepare($con, "SELECT Name FROM $table WHERE ? = SUBSTRING(UPPER(Name), 1, LENGTH(?) - ?) LIMIT 5");
+        mysqli_stmt_bind_param($stmt, "ssi", $search, $search, $offset);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $Name);
     }
 
     //Run the query, and save the results in an object
-    if ($result = mysqli_query($con, $query)) {
-        if ($table == "teachers") {
-            while ($object = mysqli_fetch_object($result)) {
-                printf("%s (%s),",$object->FullName, $object->Name);
-            }
+    if ($table == "teachers") {
+        while (mysqli_stmt_fetch($stmt)) {
+            printf("%s (%s),", $FullName, $Name);
         }
-        else {
-            while ($object = mysqli_fetch_object($result)) {
-                printf("%s,",$object->Name);
-            }
-        }
-
-        //Frees the memory used for saving the result
-        mysqli_free_result($result);
+        mysqli_stmt_close($stmt);
     }
     else {
-        echo "error";
+        while (mysqli_stmt_fetch($stmt)) {
+            printf("%s,", $Name);
+        }
+        mysqli_stmt_close($stmt);
     }
 
     //Close the connection
